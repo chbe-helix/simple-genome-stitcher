1865acc302ee5353b9a340ed70c27442f5da59ca
Chris Bennett <christopher.bennett@dshelix.com>
2019-02-11 13:53:13 -0600

    New version

--- simple_genome_stitcher.py
+++ simple_genome_stitcher.py
@@ -19,29 +19,42 @@
 # 
 
 import sys, os, subprocess, re
-import multiprocessing, codecs
+import multiprocessing
 from argparse import ArgumentParser
 
 # Set encoder for DNA string
-class GenomeCodec(codecs.Codec):
-    def encode(self, input_, errors='strict'):
-        return codecs.charmap_encode(input_, errors, encoding_table)
+def dna_encoder(seq):
+    encode = {'A' : '0',
+              'C' : '1',
+              'G' : '2',
+              'T' : '3'}
+    
+    table = str.maketrans(encode)
+    seq = seq.translate(table)
 
-    def decode(self, input_, errors='strict'):
-        return codecs.charmap_decode(input_, errors, decoding_table)
+    seq = [int(nt) for nt in seq]
+    return(seq)
+    
+    
+def ncount(plist):
+    plist = sorted(plist)
+    result = {}
 
-def lookup(name):
-    if name != 'dna':
-        return None
-    return codecs.CodecInfo(
-        name = 'dna',
-        encode=GenomeCodec().encode,
-        decode=GenomeCodec().decode)
+    ncnt, start = 1, plist[0]
+    for itr in range(len(plist)):
+        if itr+1 == len(plist):
+            result.update({ start : ncnt })
+            break
 
-decoding_table = ('A' 'C' 'G' 'T')
-encoding_table = codecs.charmap_build(decoding_table)
+        ncurr = plist[itr]
+        nnext = plist[itr+1]
+        if ncurr + 1 == nnext:
+            ncnt += 1
+        else:
+            result.update({ start : ncnt })
+            ncnt, start = 1, plist[itr+1]
 
-codecs.register(lookup)
+    return(result)
 
 # Load DNA string from fasta
 class Genome:
@@ -59,91 +72,114 @@ class Genome:
             print(ex)
 
         rseq = rseq.strip('\n').split('>')[1:]
+        while rseq:
+            nlpos = rseq[0].find('\n')
+            chrom = rseq[0][:nlpos].split()[0]
+            if 'chr' in chrom:
+                chrom = chrom.replace('chr','')
 
-        for itr in range(len(rseq)):
-            splt = rseq[itr].split()
-            self.chr_seq.update({ splt[0] : splt[-1] })
-            self.chr_len.update({ splt[0] : len(splt[-1]) })
+            self.chr_seq.update({ chrom : rseq[0][nlpos:].replace('\n','') })
+            self.chr_len.update({ chrom : len(rseq[0][nlpos:].replace('\n','')) })
+            rseq.pop(0)
 
         # del rseq
         # del splt
 
-    def _ncount(plist):
-        plist = sorted(plist)
-        result = {}
-
-        ncnt, start = 1, plist[0]
-        for itr in range(len(plist)):
-            ncurr = plist[itr]
-            nnext = plits[itr+1]
-            if ncurr + 1 == nnext:
-                ncnt += 1
-            else:
-                result.update({ start : ncnt })
-                ncnt, start = 1, plist[itr+1]
-    
     def _compress(self): # Remove N and compress size of sequence
         for chrom, seq in self.chr_seq.items():
-            npos = [i for i, ktr in enumerate(seq) if ktr == 'N']
-            npos = _ncount(npos)
+            npos = [i.start() for i in re.finditer('N', seq)]
+
+            # npos = [i for i, ktr in enumerate(seq) if ktr == 'N']
+            npos = ncount(npos)
 
             self.nidx.update({ chrom : npos })
-            self.chr_seq[chrom] = seq.replace('N', '').encode('dna')
+            self.chr_seq[chrom] = dna_encoder(seq.replace('N', ''))
 
     def extract(self):
-        _load(self)
+        self._load()
+        # self._compress()
 
-    """
-            with open(self.fn, 'r') as fp:
-                chrom, Nstr = '', False
-                n_pos = 0
 
-                for line in fp:
-                    line = line.strip()
-                    line = line.split()[0]
+def binary_compare(seq1, seq2, p1, p2, l):
+    if l > 1000:
+        while True:
+            if seq1[p1:p1+l] == seq2[p2:p2+l]:
+                p1 += l
+                p2 += l
+                continue
+            else:
+                p1, p2 = binary_compare(seq1, seq2, p1, p2, l/2)
+    else:
+        while seq1[p1] == seq2[p2]:
+            p1 += 1
+            p2 += 1
+        return(p1, p2)
+        
+def compare(seq1, seq2, l):
+    pidx = []
+    p1, p2 = 0, 0
+    while True:
+        if len(seq1[p1:]) == len(seq2[p2:]):
+            break
+        if p1+l > len(seq1) or p2+l > len(seq2):
+            l = int(l/2)
 
-                    if line.startswith('>'):
-                        chrom, Nstr = line.replace('>', ''), False
-                        n_pos = 0
-                        if 'chr' in chrom:
-                            chrom.replace('chr', '')
+        if seq1[p1:p1+l] == seq2[p2:p2+l]:
+            p1 += l
+            p2 += l
+        else:
+            p1, p2 = binary_compare(seq1, seq2, p1, p2, l/2)
+            print(p1, p2)
+            i = 1
+            while i < 15:
+                if seq1[p1+i:p1+i+50] == seq2[p2:p2+50]:
+                    p1 += i
+                    pidx.append([p1, p2])
+                    break
+                elif seq1[p1:p1+50] == seq2[p2+i:p2+i+50]:
+                    p2 += i
+                    pidx.append([p1, p2])
+                    break
+                elif seq1[p1+i:p1+i+50] == seq2[p2+i:p2+i+50]:
+                    p1 += i
+                    p2 += i
+                    break
+                i += 1
 
-                        print('Extracting chr %s' % chrom)
-                        self.chr_seq.update({ chrom : '' }) #.encode('dna') })
-                        self.chr_len.update({ chrom : 0 })
-                        self.n_idx.update({ chrom : {} })
-                        continue
-
-                    begin, end = line.find('N'), line.rfind('N') + 1
-                    assert (begin == 0 or end == len(line)) or\
-                             (begin == -1)
-
-                    if not Nstr:
-                        n_pos = self.chr_len[chrom] + len(line[:begin]) + 1
-
-                    if begin == -1:
-                        self.chr_seq[chrom] += line # .encode('dna')
-                    else:
-                        if n_pos not in self.n_idx[chrom]: 
-                            self.n_idx[chrom].update({ n_pos : 0 })
-                        
-                        self.n_idx[chrom][n_pos] += len(line[begin:end])
-                        self.chr_seq[chrom] += line[:begin] #.encode('dna')
-                        self.chr_seq[chrom] += line[end:] #.encode('dna')
-
-                    Nstr = True if end == len(line) else False
-                    self.chr_len[chrom] += len(line)
-    """
+    return(pidx)
 
 """
 """
 if __name__ == '__main__':
-    # ref = Genome('genome.fa')
-    # ref.load()
+    parser = ArgumentParser(
+        description='Simple Genome Stitcher')
+    parser.add_argument('--g1',
+                        dest='genome_1',
+                        type = str,
+                        required = True,
+                        help='First genome of interest')
+    parser.add_argument('--g2',
+                        dest='genome_2',
+                        type = str,
+                        required = True,
+                        help='Second genome of interest')
 
-    ref = open('genome.fa', 'r').read()
-    ref = ref.strip('\n').split('>')[1:]
+    args = parser.parse_args()
 
-    input('Press any key ....')
-    print("Done")
+    g1 = Genome(args.genome_1)
+    g2 = Genome(args.genome_2)
+
+    g1.extract()
+    g2.extract()
+
+    for chrom, seq in g1.chr_seq.items():
+        if chrom not in g2.chr_seq:
+            print('%s not in %s skipping' % (chrom, g2))
+            continue
+        pidx = compare(seq, g2.chr_seq[chrom], 100)
+
+        print(chrom, pidx)
+    
+    print(g1.chr_len)
+    print(g2.chr_len)
 
