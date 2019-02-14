c37420dd28dd2fe23a3bfef1ef5059fc6696c8c8
Chris Bennett <christopher.bennett@dshelix.com>
2019-02-14 11:27:52 -0600

    Full Beta Version - Still needs work

--- simple_genome_stitcher.py
+++ simple_genome_stitcher.py
@@ -105,7 +105,7 @@ class Genome:
 ### Stitcher Functions
 ###
 class SW_aligner:
-    def __init__(self, seq1, seq2, mw, mmw, gw, anc, ancw):
+    def __init__(self, seq1, seq2, mw, mmw, gw, anc, ancw, s = -1):
         if len(seq1) < len(seq2): 
             tmp = seq1
             seq1, seq2 = seq2, tmp
@@ -113,7 +113,7 @@ class SW_aligner:
 
         self.seq1       = 'Z' + seq1 # make both seq one base
         self.seq2       = 'Z' + seq2
-        self.weights    = [mw, mmw, gw, anc, ancw]# match score, mismatch score, gap score, anchor len, anchor score
+        self.weights    = [mw, mmw, gw, anc, ancw, s]# match score, mismatch score, gap score, anchor len, anchor score
         self.matrix     = None
         self.pos        = None
         self.alignments = None
@@ -122,7 +122,7 @@ class SW_aligner:
     def _SWmatrix(self): # sequences, match score, mismatch score, gap score
         # Make SW matrix
         seq1, seq2 = self.seq1, self.seq2
-        m, mm, g, a, aw = self.weights
+        m, mm, g, a, aw, s = self.weights
  
         cols, rows = len(seq1), len(seq2)
         score_matrix = [[0 for col in range(cols)] for row in range(rows)]
@@ -144,6 +144,8 @@ class SW_aligner:
                     max_pos   = (i, j)
 
                 if (i == j) and (i <= a and j <= a):
+                    score_matrix[i][j] = score + aw
+                elif (s != -1) and (i == j) and (i > rows-s and j > cols-s):
                     score_matrix[i][j] = score + aw
                 else:
                     score_matrix[i][j] = score
@@ -224,6 +226,37 @@ class SW_aligner:
     def get_matrix(self):
         return self.matrix
 
+def kmer_compare(seq1, seq2, p1, p2, k):
+    kmers1, kmers2 = [], []
+    p1a, p2a = 0, 0 # Additions to 1 and 2
+
+    i = 0
+    while i < len(seq1) - k:
+        kmer1, kmer2 = seq1[i:i+k], seq2[i:i+k]
+        
+        match = False
+        if kmer1 == kmer2:
+            p1a += i
+            p2a += i
+            match = True
+        elif kmer1 in kmers2:
+            p1a += i
+            p2a += kmers2.index(kmer1)
+            match = True
+        elif kmer2 in kmers1:
+            p1a += kmers1.index(kmer2)
+            p2a += i
+            match = True
+
+        if match:
+            return(True, p1a, p2a)
+
+        i += 1
+        kmers1.append(kmer1)
+        kmers2.append(kmer2)
+
+    print('No match in kmer size %s in window %s' % (k, len(seq1)))
+    return(False, p1a, p1a)
 
 def binary_compare(seq1, seq2, p1, p2, l):
     p1, p2, l = int(p1), int(p2), int(l)
@@ -241,7 +274,7 @@ def binary_compare(seq1, seq2, p1, p2, l):
             p2 += 1
     
     return(p1, p2)
-        
+
 def compare(seq1, seq2, l, s, isize):
     pidx = []
     p1, p2 = 0, 0
@@ -276,51 +309,98 @@ def compare(seq1, seq2, l, s, isize):
                     break
                 i += 1
 
-                if i == isize:
-                    m, mm, g, aw = 3, -1, -1, 5
-                    b, e = 10, 30
-                    aseq1, aseq2 = '', ''
-                    while e < 200:
-                        aln = SW_aligner(seq2[p2-b:p2+(e*2)], seq1[p1-b:p1+e], m, mm, g, b, aw)
-                        aln.dyn_align()
-                        aln_str = aln.get_align_str()[0]
-                        if aln_str.endswith('|' * s):
-                            aseq2, aseq1 = aln.get_seq_align()
-                            break
-                        e += s
-                    if not aseq1 or not aseq2:
-                        print('No matches: trying to increase insert size and seed')
-                        if isize > 1000000:
-                            print('Insert size over one million: Exiting')
-                            exit(1)
-                        isize += 1000
-                        s += 15
+            if i == isize:
+                m, mm, g, aw = 3, -1, -1, 10 # SW Weights: match, missmatch, gap, anchor
+                b, e = 10, 30               # Bases to take at Begin and End of p1 and p2 site
+                k = 50                   # K-mer size
+                aseq1, aseq2 = '', ''
+                while e < 200:
+                    aln = SW_aligner(seq2[p2-b:p2+(e*2)], seq1[p1-b:p1+e], m, mm, g, b, aw)
+                    aln.dyn_align()
+                    aln_str = aln.get_align_str()[0]
+                    if aln_str.endswith('|' * s):
+                        aseq2, aseq1 = aln.get_seq_align()
+                        break
+                    e += s
+
+                if not aseq1 or not aseq2:
+                    print('No matches: trying k-mer matching')
+                    ws = 5000 # window size for kmer comparison
+                    found = False
+                    while ws < 100000 and not found: 
+                        found, p1a, p2a = kmer_compare(seq1[p1:p1+ws], seq2[p2:p2+ws], p1, p2, k)
+                        ws += 5000
+
+                    if p1a == 0 or p2a == 0:
+                        print('K-mer matching found insert. Setting Genome_1 to %d and Genome_2 to %d' % (p1+p1a, p2+p2a))
+                        p1 += p1a
+                        p2 += p2a
+                        pidx.append([p1, p2])
                         continue
 
-                    r2 = [p2-b, p2+(e*2)]
-                    r1 = [p1-b, p1+e]
-                    # print('Aligning Regions:%s and %s in seq2 and seq 1\n%s\n%s\n%s' % (r2, r1, aseq2, aln_str, aseq1))
+                    if found:
+                        print('K-mer match found at distances of %d in Genome_1 and %d in Genome_2. Resuming indexing' % (p1a, p2a))
+                        gg1 = True if p1a > p2a else False
+                        p1ckr, p2ckr = p1 + p1a, p2 + p2a
+                        p1 -= 1; p2 -= 1
+                        p1a += 1; p2a += 1
 
-                    if aln_str.startswith('|' * b):
-                        p1 -= b
-                        p2 -= b
-                    else:
-                        print('Max insert size reached and no definitive match: p1: %s, p2:%s' % (p1, p2))
-                        exit(1)                    
-
-                    for ntidx in range(len(aln_str)):
-                        c = aln_str[ntidx]
-                        nt1, nt2 = aseq1[ntidx], aseq2[ntidx]
-                        if c != '~':
-                            p1 += 1
-                            p2 += 1
-                        else:
-                            if nt1 == '-':
+                        if gg1:
+                            ratio = int(p1a/p2a)
+                            remain = p1a%p2a
+                            while p2a > 0:
                                 p2 += 1
-                            if nt2 == '-':
+                                p2a -= 1
+                                if p2a == 0:
+                                   p1 += (ratio+remain)
+                                else:
+                                    p1 += (ratio)
+                                    
+                                pidx.append([p1, p2])
+                                
+                        else:
+                            ratio = int(p2a/p1a)
+                            remain = p2a%p1a
+                            while p1a > 0:
                                 p1 += 1
-                            pidx.append([p1, p2])
-                    break
+                                p1a -= 1
+
+                                if p1a == 0:
+                                   p2 += (ratio+remain)
+                                else:
+                                    p2 += (ratio)
+                                pidx.append([p1, p2])
+                                        
+                        assert p1ckr == p1 and p2ckr == p2, '%s : %d != %d, %d != %d' % (gg1, p1ckr, p1, p2ckr, p2)
+                        continue
+
+                    else:
+                        print('No K-mer match found. Reevaluate options')
+                        exit(1)
+
+                r2 = [p2-b, p2+(e*2)]
+                r1 = [p1-b, p1+e]
+                print('Aligning Regions:%s and %s in seq2 and seq 1\n%s\n%s\n%s' % (r2, r1, aseq2, aln_str, aseq1))
+
+                if aln_str.startswith('|' * b):
+                    p1 -= b
+                    p2 -= b
+                else:
+                    print('Max insert size reached and no definitive match: p1: %s, p2:%s' % (p1, p2))
+                    exit(1)                    
+
+                for ntidx in range(len(aln_str)):
+                    c = aln_str[ntidx]
+                    nt1, nt2 = aseq1[ntidx], aseq2[ntidx]
+                    if c != '~':
+                        p1 += 1
+                        p2 += 1
+                    else:
+                        if nt1 == '-':
+                            p2 += 1
+                        if nt2 == '-':
+                            p1 += 1
+                        pidx.append([p1, p2])
 
     return(pidx)
 
@@ -328,8 +408,9 @@ def check(g1, g2, chrom_idx):
     g1.load_idx()
     g2.load_idx()
 
+    passed = True
     for chrom, ls in g1.fai.items():
-        if chrom not in g2.chr_len or chrom == '6':
+        if chrom not in g2.chr_len:
             print('Skipping %s. Not in Genome_2' % chrom)
             continue
         ls1 = int(ls[0])
@@ -338,8 +419,10 @@ def check(g1, g2, chrom_idx):
 
         if ls1 != int(g1.chr_len[chrom]):
             print('Chr %s Failed length test in genome_1' % chrom)
+            passed = False
         if ls2 != int(g2.chr_len[chrom]):
             print('Chr %s Failed length test in genome_2' % chrom)
+            passed = False
 
         if not chrom_idx[chrom]:
             print('Chr %s NO CHANGE' % chrom)
@@ -350,8 +433,28 @@ def check(g1, g2, chrom_idx):
 
         if kdiff != diff:
             print('Chr %s FAILED: index conversion of know diff %d and calculated diff %d' % (chrom, kdiff, diff))
+            passed = False
         else: 
             print('Chr %s PASSED' % chrom)
+
+    return(passed)
+
+
+def build_idx(g1, g2, chrom_idx):
+    g1name = g1.fn.split('.')[0]
+    g2name = g2.fn.split('.')[0]
+    with open('%s-%s-coord.idx' % (g1name, g2name), 'w') as fo:
+        fo.write('## Coordinant index for %s vs %s\n' % (g1name, g2name))
+        fo.write('#ORD\t%s\t%s\n' % (g1name, g2name))
+        for chrom, index in chrom_idx.items():
+            entry = ''
+            entry += chrom + '\t'
+            entry += g1.fai[chrom][0] + '\t'
+            entry += g2.fai[chrom][0] + '\t'
+            for i in index:
+                entry += '%d:%d,' % (i[0], i[1])
+            entry = entry[:-1] + '\n'
+            fo.write(entry)
 
 """
 """
@@ -382,7 +485,7 @@ if __name__ == '__main__':
                         dest='insert_size',
                         type = int,
                         default = 250,
-                        help='Max size of insert allowed (Default: 15)')
+                        help='Base size of insert allowed before alternative methods (Default: 250)')
 
     args = parser.parse_args()
 
@@ -397,12 +500,17 @@ if __name__ == '__main__':
         if chrom not in g2.chr_seq:
             print('%s not in %s skipping' % (chrom, g2))
             continue
+            
+        print('Indexing chrom %s' % chrom)        
 
-        if chrom == '6':
-            print("Chromosome 6 is very problematic: Skipping")
-            continue
         pidx = compare(seq, g2.chr_seq[chrom], args.window_size, args.seed_size, args.insert_size)
 
         chrom_idx.update({ chrom : pidx })
 
-    check(g1, g2, chrom_idx)
+    passed = check(g1, g2, chrom_idx)
+
+    if not passed:
+        print('At least one failure. Not building index')
+        exit(1)
+
+    build_idx(g1, g2, chrom_idx)
